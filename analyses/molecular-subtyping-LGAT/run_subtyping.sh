b9db0f66e0b9c5a8ef0816c1be0f35db3b8aedfb
Alex Sickler <sicklera@chop.edu>
2021-08-09 09:56:25 -0400

    :build_construction: stop making LGAT json file and move it to inputs

--- analyses/molecular-subtyping-LGAT/run_subtyping.sh
+++ analyses/molecular-subtyping-LGAT/run_subtyping.sh
@@ -5,14 +5,11 @@ set -o pipefail
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-# This option controls whether on not the step that generates the LGAT only mutation 
+# This option controls whether on not the step that generates the LGAT only mutation
 # files gets run -- it will be turned off in CI
 SUBSET=${OPENPBTA_SUBSET:-1}
 
-# Generate JSON file with strings for inclusion/exclusion criteria 
-Rscript --vanilla 00-LGAT-select-pathology-dx.R
-
-# subset by SNV 
+# subset by SNV
 Rscript -e "rmarkdown::render('01-subset-files-for-LGAT.Rmd')"
 # subset by Fusion
 Rscript -e "rmarkdown::render('02-subset-fusion-files-LGAT.Rmd')"
