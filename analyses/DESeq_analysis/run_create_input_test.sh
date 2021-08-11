b6f5b81d14ab393ad250bf00e83143a43f7daa46
sangeetashukla <82055098+sangeetashukla@users.noreply.github.com>
2021-08-11 08:49:34 -0400

    Changed a script name and removed typos

--- analyses/DESeq_analysis/run_create_input_test.sh
+++ analyses/DESeq_analysis/run_create_input_test.sh
@@ -21,17 +21,17 @@ mkdir -p test
 
 #This script creates a subset of the histologies.tsv, to use for testing the deseq module.
 
-Rscript --vanilla create_test_input.R \
-        --hist_file ../../data/v7/histologies_original.tsv \
-        --counts_file ../../data/v7/gene-counts-rsem-expected_count-collapsed.rds \
+Rscript --vanilla process_test_input.R \
+        --hist_file ../../data/histologies_original.tsv \
+        --counts_file ../../data/gene-counts-rsem-expected_count-collapsed.rds \
         --outdir test
 
 
 Rscript --vanilla run-DESeq-analysis.R \
         --hist_file test/histologies_subset.tsv \
-        --counts_file ../../data/v7/gene-counts-rsem-expected_count-collapsed.rds \
-        --tpm_file ../../data/v7/gene-expression-rsem-tpm-collapsed.rds  \
-        --efo_mondo_file ../../data/v7/efo-mondo-map.tsv \
-        --gtex_subgroup_uberon ../../data/v7/uberon-map-gtex-subgroup.tsv \
-        --ensg_hugo_file ../../data/v7/ensg-hugo-rmtl-mapping.tsv \
+        --counts_file ../../data/gene-counts-rsem-expected_count-collapsed.rds \
+        --tpm_file ../../data/gene-expression-rsem-tpm-collapsed.rds  \
+        --efo_mondo_file ../../data/efo-mondo-map.tsv \
+        --gtex_subgroup_uberon ../../data/uberon-map-gtex-subgroup.tsv \
+        --ensg_hugo_file ../../data/ensg-hugo-rmtl-mapping.tsv \
         --outdir results        
