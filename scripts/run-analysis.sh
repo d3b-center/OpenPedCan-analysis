6aa2621e985e2717da4e04b06be9ff267acc6cf3
devbyaccident <blackdenc@chop.edu>
2022-05-04 14:13:38 -0400

    👷 Adding run_analysis workflow

---
+++ scripts/run-analysis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+script_directory="$(perl -e 'use File::Basename;
+ use Cwd "abs_path";
+ print dirname(abs_path(@ARGV[0]));' -- "$0")"
+cd "$script_directory" || exit
+
+# However in order to give Docker access to all the code we have to
+# move up a level
+cd ..
+
+env | grep "OPENPBTA_.*" > open_pbta_envs.txt
+
+docker run \
+       --env-file=open_pbta_envs.txt \
+       --volume "$(pwd)":/rocker-build/ \
+       -it "openpedcan-analysis:latest" "$@"
