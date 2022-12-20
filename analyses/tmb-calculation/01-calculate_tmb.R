5c2103a6a8c15cf4fdb7b86e729bb64f82cc7aa7
Alex Sickler <sicklera@chop.edu>
2022-12-20 10:22:20 -0500

    :rotating_light: lint calculate tmb script

--- analyses/tmb-calculation/01-calculate_tmb.R
+++ analyses/tmb-calculation/01-calculate_tmb.R
@@ -1,10 +1,10 @@
-# Calculate TMB for a given SNV consensus MAF file from Strelka2, Mutect2, 
+# Calculate TMB for a given SNV consensus MAF file from Strelka2, Mutect2,
 # Vardict, data Lancet callers
 #
 # Eric Wafula for Pediatric OpenTargets
 # 12/10/2021
 # Adapted from AlexsLemonade OpenPBTA-analysis snv-callers analysis module
-# 
+#
 
 # Load libraries:
 suppressPackageStartupMessages(library(optparse))
@@ -14,32 +14,39 @@ suppressPackageStartupMessages(library(GenomicRanges))
 
 # set up optparse options
 option_list <- list(
-  make_option(opt_str = "--consensus_maf_file", type = "character", default = NULL,
-              help = "Input OpenPedCan SNV consensus consensus MAF file.",
-              metavar = "character"
-              ),
-  make_option(opt_str = "--bed_files", type = "character", default = NULL,
-              help = "Input samples to target BED mapping file."),
-  make_option(opt_str = "--histologies_file", type = "character", default = NULL, 
-              help = "Input OpenPedCan histologies metadata file.",
-              metavar = "character"
-              ),  
-  make_option(opt_str = "--coding_regions", type = "character",
-              help = "BED file for coding regions to use for coding only TMB.",
-              metavar = "character"
-              ),
-  make_option(opt_str = "--nonsynfilter_maf", action = "store_true", default = FALSE, 
-              help = "If TRUE, filter out synonymous mutations, keep 
+  make_option(
+    opt_str = "--consensus_maf_file", type = "character", default = NULL,
+    help = "Input OpenPedCan SNV consensus consensus MAF file.",
+    metavar = "character"
+  ),
+  make_option(
+    opt_str = "--bed_files", type = "character", default = NULL,
+    help = "Input samples to target BED mapping file."
+  ),
+  make_option(
+    opt_str = "--histologies_file", type = "character", default = NULL,
+    help = "Input OpenPedCan histologies metadata file.",
+    metavar = "character"
+  ),
+  make_option(
+    opt_str = "--coding_regions", type = "character",
+    help = "BED file for coding regions to use for coding only TMB.",
+    metavar = "character"
+  ),
+  make_option(
+    opt_str = "--nonsynfilter_maf", action = "store_true", default = FALSE,
+    help = "If TRUE, filter out synonymous mutations, keep
               non-synonymous mutations, according to maftools definition.
               Default is FALSE",
-              metavar = "character"
-              ),
-  make_option(opt_str = "--nonsynfilter_focr", action = "store_true", default = FALSE,
-              help = "If TRUE, filter out synonymous mutations, keep 
-              non-synonymous mutations, according to Friends of Cancer 
+    metavar = "character"
+  ),
+  make_option(
+    opt_str = "--nonsynfilter_focr", action = "store_true", default = FALSE,
+    help = "If TRUE, filter out synonymous mutations, keep
+              non-synonymous mutations, according to Friends of Cancer
               Research definition. Default is FALSE",
-              metavar = "character"
-              )
+    metavar = "character"
+  )
 )
 
 # parse parameter options
@@ -61,10 +68,14 @@ results_dir <- file.path(module_dir, "results")
 input_dir <- file.path(module_dir, "input")
 
 # source functions splitting MNVs and computing TMB scores
-source(file.path(root_dir, "analyses", "tmb-calculation", 
-                 "util", "split_mnv.R"))
-source(file.path(root_dir, "analyses", "tmb-calculation", 
-                 "util", "tmb_functions.R"))
+source(file.path(
+  root_dir, "analyses", "tmb-calculation",
+  "util", "split_mnv.R"
+))
+source(file.path(
+  root_dir, "analyses", "tmb-calculation",
+  "util", "tmb_functions.R"
+))
 
 # Create results folder if it doesn't exist
 if (!dir.exists(results_dir)) {
@@ -127,12 +138,12 @@ focr_nonsynonymous <- c(
 )
 
 # variant filters
-vaf_cutoff = 0.05
-var_count = 3
-tumor_depth = 25
+vaf_cutoff <- 0.05
+var_count <- 3
+tumor_depth <- 25
 
 # filter create the consensus MAF for SNV calls (non-MNV)
-snv_maf_df <- consensus_maf_df %>%  
+snv_maf_df <- consensus_maf_df %>%
   dplyr::filter(!Variant_Type %in% c("DNP", "TNP", "ONP"))
 
 # get MNV calls from the consensus MAF file and split into SNV calls
@@ -140,51 +151,63 @@ split_mnv_maf_df <- split_mnv(consensus_maf_df) %>%
   dplyr::select(-c(mnv_id, mnv_pos))
 
 # Merge SNV calls with split MNV calls
-snv_mnv_maf_df <- rbind(snv_maf_df, split_mnv_maf_df) %>% 
-  distinct(Chromosome, Start_Position, Reference_Allele, Allele, 
-           Tumor_Sample_Barcode, .keep_all= TRUE)
-           
+snv_mnv_maf_df <- rbind(snv_maf_df, split_mnv_maf_df) %>%
+  distinct(Chromosome, Start_Position, Reference_Allele, Allele,
+    Tumor_Sample_Barcode,
+    .keep_all = TRUE
+  )
+
 # If the maftools non-synonymous filter is on, filter out synonymous mutations
 if (nonsynfilter_maf) {
   snv_mnv_maf_df <- snv_mnv_maf_df %>%
-    dplyr::filter(Variant_Classification %in% maf_nonsynonymous,
-                  t_depth >= tumor_depth,
-                  vaf >= vaf_cutoff,
-                  t_alt_count >= var_count)
+    dplyr::filter(
+      Variant_Classification %in% maf_nonsynonymous,
+      t_depth >= tumor_depth,
+      vaf >= vaf_cutoff,
+      t_alt_count >= var_count
+    )
 }
 
-# If the FoCR non-synonymous filter is on, filter out synonymous mutations 
+# If the FoCR non-synonymous filter is on, filter out synonymous mutations
 # according to that definition
 if (nonsynfilter_focr) {
   snv_mnv_maf_df <- snv_mnv_maf_df %>%
-    dplyr::filter(Variant_Classification %in% focr_nonsynonymous,
-                  t_depth >= tumor_depth,
-                  vaf >= vaf_cutoff,
-                  t_alt_count >= var_count)
+    dplyr::filter(
+      Variant_Classification %in% focr_nonsynonymous,
+      t_depth >= tumor_depth,
+      vaf >= vaf_cutoff,
+      t_alt_count >= var_count
+    )
 }
 
 ########################### Set up metadata columns ############################
 message("Setting up metadata...\n")
 
-# load samples to target BED mapping file 
+# load samples to target BED mapping file
 bed_df <- readr::read_tsv(bed_files)
 # assert all sample ids are not NA
-stopifnot(identical(sum(is.na(bed_df$Kids_First_Biospecimen_ID)), 
-                    as.integer(0)))
+stopifnot(identical(
+  sum(is.na(bed_df$Kids_First_Biospecimen_ID)),
+  as.integer(0)
+))
 # assert all sample ids are unique
-stopifnot(identical(length(bed_df$Kids_First_Biospecimen_ID), 
-                    length(unique(bed_df$Kids_First_Biospecimen_ID))))
-# load histologies file  
+stopifnot(identical(
+  length(bed_df$Kids_First_Biospecimen_ID),
+  length(unique(bed_df$Kids_First_Biospecimen_ID))
+))
+# load histologies file
 hist_df <- readr::read_tsv(histologies_file, guess_max = 10000)
 
 # merge samples to target BED mapping file with select metadata from
 # histologies file
-metadata_df <- bed_df %>% 
-  dplyr::inner_join(hist_df %>% 
-                      dplyr::select(
-                        Kids_First_Biospecimen_ID, 
-                        experimental_strategy), 
-                    by = "Kids_First_Biospecimen_ID") %>% 
+metadata_df <- bed_df %>%
+  dplyr::inner_join(hist_df %>%
+    dplyr::select(
+      Kids_First_Biospecimen_ID,
+      experimental_strategy
+    ),
+  by = "Kids_First_Biospecimen_ID"
+  ) %>%
   dplyr::rename(
     Tumor_Sample_Barcode = Kids_First_Biospecimen_ID,
     target_bed = bed_to_use
@@ -192,18 +215,21 @@ metadata_df <- bed_df %>%
 
 # Isolate metadata to only the samples that are in the mutation dataset
 metadata_df <- metadata_df %>% dplyr::filter(
-  Tumor_Sample_Barcode %in% unique(snv_mnv_maf_df$Tumor_Sample_Barcode)) %>%
+  Tumor_Sample_Barcode %in% unique(snv_mnv_maf_df$Tumor_Sample_Barcode)
+) %>%
   # Make a Target BED regions file path column
-  #dplyr::mutate(target_bed_path =  file.path(input_dir, target_bed)) %>% 
+  # dplyr::mutate(target_bed_path =  file.path(input_dir, target_bed)) %>%
   dplyr::mutate(
-    target_bed_path = 
+    target_bed_path =
       dplyr::recode(target_bed,
-                    "intersect_strelka_mutect2_vardict_WGS.bed" = 
-                      file.path(root_dir, "scratch/intersect_strelka_mutect2_vardict_WGS.bed"),
-                    .default = file.path(data_dir, target_bed)))
+        "intersect_strelka_mutect2_vardict_WGS.bed" =
+          file.path(root_dir, "scratch/intersect_strelka_mutect2_vardict_WGS.bed"),
+        .default = file.path(data_dir, target_bed)
+      )
+  )
 
 # exclude samples in that are in the mutation dataset and not the metadata
-# ideally this should not happen but is currently the case in the v10 
+# ideally this should not happen but is currently the case in the v10
 # mutation dataset
 snv_mnv_maf_df <- snv_mnv_maf_df %>%
   dplyr::filter(Tumor_Sample_Barcode %in% metadata_df$Tumor_Sample_Barcode)
@@ -211,13 +237,13 @@ snv_mnv_maf_df <- snv_mnv_maf_df %>%
 # Add in metadata
 snv_mnv_maf_df <- snv_mnv_maf_df %>%
   dplyr::inner_join(metadata_df %>%
-                      dplyr::select(
-                        Tumor_Sample_Barcode,
-                        experimental_strategy,
-                        target_bed,
-                        target_bed_path
-                      ),
-                    by = "Tumor_Sample_Barcode"
+    dplyr::select(
+      Tumor_Sample_Barcode,
+      experimental_strategy,
+      target_bed,
+      target_bed_path
+    ),
+  by = "Tumor_Sample_Barcode"
   ) %>%
   # Remove samples if they are not WGS or WXS
   dplyr::filter(experimental_strategy %in% c("WGS", "WXS", "Targeted Sequencing"))
@@ -240,7 +266,8 @@ bed_ranges_list <- lapply(bed_file_paths, function(bed_file) {
 
   # Read in BED file as data.frame
   bed_df <- readr::read_tsv(bed_file,
-    col_names = c("chr", "start", "end"))
+    col_names = c("chr", "start", "end")
+  )
 
   # Make into a GenomicRanges object
   bed_ranges <- GenomicRanges::GRanges(
@@ -258,7 +285,8 @@ message("Setting up coding regions BED ranges...\n")
 
 # Read in the coding regions BED file
 coding_regions_df <- readr::read_tsv(coding_regions,
-  col_names = c("chr", "start", "end"))
+  col_names = c("chr", "start", "end")
+)
 
 # Make into a GenomicRanges object
 coding_ranges <- GenomicRanges::GRanges(
@@ -294,8 +322,8 @@ tmb_all_df <- purrr::map2_df(
     tumor_sample_barcode = .x,
     maf_df = snv_mnv_maf_df,
     bed_ranges = bed_ranges_list[[.y]]
-    )
   )
+)
 
 # Write to TSV file
 readr::write_tsv(tmb_all_df, tmb_all_file)
@@ -314,8 +342,8 @@ tmb_coding_df <- purrr::map2_df(
     tumor_sample_barcode = .x,
     maf_df = snv_mnv_maf_df,
     bed_ranges = coding_bed_ranges_list[[.y]]
-    )
   )
+)
 
 # Write to TSV file
 readr::write_tsv(tmb_coding_df, tmb_coding_file)
